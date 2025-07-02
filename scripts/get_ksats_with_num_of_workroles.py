7cde5899c1dcaba5c03beb15b601c72f590689a1
Paige <pz0342@gmail.com>
2025-07-02 12:36:17 -0400

    Work Role Breakdown

--- scripts/get_ksats_with_num_of_workroles.py
+++ scripts/get_ksats_with_num_of_workroles.py
@@ -6,14 +6,17 @@ def read_json_file(input_path):
         data = json.load(infile)
     return data
 
-def process_data(ku_data):
+def process_data(ku_data, top_range):
     """
     Placeholder for processing logic.
     Modify this function based on your data structure and processing needs.
     """
+    print_ksats = []
+    for ksat in ku_data:
+        if len(ksat["work_roles"]) >= top_range - 1 and len(ksat["work_roles"]) <= top_range:
+            print_ksats.append(ksat)
     
-    
-    return ku_data
+    return print_ksats
 
 def write_json_file(output_path, data):
     """Writes data to a JSON file."""
@@ -21,11 +24,16 @@ def write_json_file(output_path, data):
         json.dump(data, outfile, indent=4)
 
 if __name__ == "__main__":
-    wr_input_file = 'dcwf/ksat_with_work_role_names.json'
-    output_file = 'dcwf_ksats_with_35_or_more_work_roles.json'
+    wr_input_file = 'nice/ksat_with_work_role_names.json'
 
     ku_raw_data = read_json_file(wr_input_file)
-    processed_data = process_data(ku_raw_data)
-    write_json_file(output_file, processed_data)
+    i = 72
+    while i > 0: 
+        output_file = f'nice/nice_ksats_with_{i}_work_roles.json'
+        print(i)
+        processed_data = process_data(ku_raw_data, i)
+        if len(processed_data) > 0:
+            write_json_file(output_file, processed_data)
+        i = i - 1
 
     print(f"Processed data has been written to {output_file}")
